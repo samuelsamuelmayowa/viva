39126312df6595bf88dc9e24cea960d7dfe63416
mayowasamuel <fpasamuelmayowa51@gmail.com>
2026-09-25 08:31:24 +0100

    final fixgit      add . !

--- viva/src/lib/api.js
+++ viva/src/lib/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://vivabackend.vercel.app/api",
+const API_URL = import.meta.env.VITE_API_URL || "https://vivabackend.vercel.app";
 
+export const api = axios.create({
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
   timeout: 20000,
 });
